1e9706f9757185b8bf3e1a9e7720ec9f95a6efdf
Dharanish-AM <dharanish816@gmail.com>
2026-05-14 10:29:33 +0530

    Add developer dashboard and HomeController
    Introduce a developer-facing dashboard for the Parking Payment Service: add HomeController (renders the view) and a fully client-side home.scala.html UI to create/payments, show lifecycle/status, and display API responses. Update routes to map GET / to the new controller and remove the default public/index.html. Adjust application.conf to allow localhost hosts and disable the CSRF filter for this developer UI (convenience for local testing).

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,14 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+import scala.concurrent.ExecutionContext
+
+@Singleton
+class HomeController @Inject()(cc: ControllerComponents)(implicit ec: ExecutionContext)
+    extends AbstractController(cc) {
+
+  def index: Action[AnyContent] = Action {
+    Ok(views.html.home()).as(HTML)
+  }
+}
